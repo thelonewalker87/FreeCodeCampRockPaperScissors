8fd5dc555fe065b21d19b4766020eeb4edd8d6fc
Ahaan Dhar <the.lonewalker07@gmail.com>
2026-06-09 14:30:23 +0530

    Add complete Rock Paper Scissors project files and solution

--- RPS.py
+++ RPS.py
@@ -1,10 +1,22 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+def player(prev_play, opponent_history=[], play_order={}):
+    if not prev_play:
+        opponent_history.clear()
+        return "R"
 
-def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
+    n = 3 
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(opponent_history) > n:
+        pattern = "".join(opponent_history[-(n + 1):])
+        play_order[pattern] = play_order.get(pattern, 0) + 1
 
-    return guess
+    last_n_moves = "".join(opponent_history[-n:])
+    
+    prediction = max(
+        ["R", "P", "S"], 
+        key=lambda move: play_order.get(last_n_moves + move, 0)
+    )
+
+    ideal_response = {"R": "P", "P": "S", "S": "R"}
+    
+    return ideal_response[prediction]
